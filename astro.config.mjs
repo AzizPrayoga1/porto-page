a63d711e139d641d520a4cc64155d78a710cb64e
AzizPrayoga1 <prayogaaziz873@gmail.com>
2026-07-23 13:35:42 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     react(),
     tailwind()
   ],
-  output: 'static'
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
 });
